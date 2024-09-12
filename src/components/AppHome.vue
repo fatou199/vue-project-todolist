b32e2570566e67c467b34757ab53aad66df3303d
fatou199 <65873642+fatou199@users.noreply.github.com>
2024-09-12 16:28:06 +0200

    add taskList component and import it in AppHome

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     <h1>Bienvenue sur la page d'accueil</h1>
+
+    <!-- Intégration de la liste des taches -->
+    <task-list></task-list>
   </div>
 </template>
+
+<script>
+// Appel du composant TaskList
+import TaskList from './task/TaskList.vue'
+
+export default {
+  components: {
+    TaskList
+  }
+}
+</script>
---
+++ src/components/task/TaskList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- Champ de saisie pour ajouter une nouvelle tâche -->
+    <input v-model="newTask" placeholder="Ajouter une tâche" />
+
+    <!-- Bouton pour ajouter une nouvelle tâche -->
+    <button @click="addTask">Ajouter</button>
+
+    <!-- Liste des tâches -->
+    <ul>
+      <!-- Pour chaque tache m'afficher un element de liste -->
+      <!-- v-bind:key donne une clé unique à chaque tâche -->
+      <li v-for="(task, index) in tasks" v-bind:key="task.id">
+        <!-- Checkbox pour marquer la tache comme faite -->
+        <input type="checkbox" v-model="task.done" />
+        <!-- Affichage du nom de la tache -->
+        <span>{{ task.name }}</span>
+        <!-- Bouton pour modifier et supprimer la tache -->
+        <button @click="updateTask(index)">Modifier</button>
+        <button @click="deleteTask(index)">Supprimer</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: [{ name: 'Ma première tâche' }]
+    }
+  }
+}
+</script>
