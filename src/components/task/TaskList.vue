5c9e54782ec85f4e87968316b4d41235d14a1481
fatou199 <65873642+fatou199@users.noreply.github.com>
2024-09-12 18:30:54 +0200

    add localStorage

--- src/components/task/TaskList.vue
+++ src/components/task/TaskList.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="box">
+    <!-- Ajouter des taches -->
     <!-- Champ de saisie pour ajouter une nouvelle tâche -->
     <input v-model="newTask" placeholder="Ajouter une tâche" />
 
     <!-- Bouton pour ajouter une nouvelle tâche -->
     <button @click="addTask">Ajouter</button>
 
-    <!-- Liste des tâches -->
+    <!-- Condition pour afficher le nombre de tache -->
+    <p v-if="tasks.length === 0">Aucune tâche à afficher</p>
+    <p v-else>Nombre de tâches : {{ tasks.length }}</p>
+
+    <!-- Affichage de la liste des tâches -->
     <ul>
       <!-- Pour chaque tache m'afficher un element de liste -->
       <!-- v-bind:key donne une clé unique à chaque tâche -->
@@ -19,10 +24,11 @@
           <button @click="cancelTask(index)">Annuler</button>
         </div>
 
+        <!-- Si la tache n'est pas en mode edition -->
         <div class="list" v-else>
           <div class="task-container">
             <!-- Checkbox pour marquer la tache comme terminée -->
-            <input type="checkbox" v-model="task.done" />
+            <input type="checkbox" v-model="task.done" @change="storageTasks" />
             <!-- Texte de la tâche, barré si elle est terminée -->
             <span v-bind:class="{ completed: task.done }">{{ task.name }}</span>
           </div>
@@ -45,11 +51,15 @@ export default {
       // Variable pour stocker le texte de la nouvelle tâche
       newTask: '',
       // Tableau vide pour stocker les tâches
-      tasks: []
+      // Récupère les tâches sauvegardées dans le Local Storage
+      // Si aucune tâche n'est sauvegardée, le tableau est vide par défaut
+      // JSON.parse pour convertir les données en tableau
+      tasks: JSON.parse(localStorage.getItem('tasks')) || []
     }
   },
 
   methods: {
+    // Ajouter une tâche
     addTask() {
       // verifie si le champ contient une valeur ou non
       if (this.newTask) {
@@ -64,8 +74,8 @@ export default {
           // Par defaut la tache n'est pas terminée
           done: false
         })
-        // Reinitialise le champ de saisie
-        this.newTask = ''
+        this.newTask = '' // Reinitialise le champ de saisie
+        this.storageTasks() // Sauvegarde dans le Local Storage
       }
     },
 
@@ -77,15 +87,23 @@ export default {
     // Mettre à jour la tache
     updateTask(index) {
       this.tasks[index].isEdit = false
+      this.storageTasks()
     },
 
+    // Annuler la modification de la tâche
     cancelTask(index) {
       this.tasks[index].isEdit = false
+      this.storageTasks()
     },
 
     // Supprimer une tâche
     deleteTask(index) {
       this.tasks.splice(index, 1)
+    },
+
+    // Sauvegarde les tâches dans le Local Storage
+    storageTasks() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
   }
 }
